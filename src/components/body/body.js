0f0bbd6c39bdc7874798296d68a82ca98f356e10
MD Rafsan <shazidno123@gmail.com>
2022-02-19 21:54:28 +0600

    add

---
+++ src/components/body/body.js
@@ -0,0 +1,14 @@
+
+import React, { Component } from 'react';
+import clasess from './body.moduler.css';
+
+
+const body = () => {
+    return (
+        <div className={clasess.mainBody}>
+
+        </div>
+    )
+}
+
+export default body
