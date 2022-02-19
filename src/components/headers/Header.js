c1357b1fe9fde0027aef4f4023586d360faddc25
MD Rafsan <shazidno123@gmail.com>
2022-02-19 13:11:42 +0600

    start_new_file

---
+++ src/components/headers/Header.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import classes from './Header.module.css';
+import logo from '../../assets/images/logo.svg';
+import reactlogo from '../../assets/images/react.svg';
+
+const Header = () => {
+    return (
+        <header>
+            <div className='container'>
+                <div>
+                    <img className={classes.logo} src={logo} />
+                </div>
+                <div class="textRight">
+                    <img src={reactlogo} alt="React" className={classes.reactLogo} /><strong>React</strong>
+                </div>
+            </div>
+
+
+        </header>
+    )
+}
+
+export default Header
