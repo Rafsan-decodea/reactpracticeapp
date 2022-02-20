8fb32c7d20f705a9ad2a246f03f1dc6723e7646f
MD Rafsan <shazidno123@gmail.com>
2022-02-20 12:08:01 +0600

    adding data

---
+++ src/components/builder/builder.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+function builder() {
+    return (
+        <div class="react" id="builder">
+            <div class="builder">
+                <h3>Build your own Ice Cream Sundae</h3>
+
+                {/* iteams */}
+
+                {/* <!-- total price start --> */}
+
+                {/* total price */}
+
+                {/* <!-- total price end --> */}
+                <button type="button" onclick="next()" class="order rounded">
+                    Add to Cart
+                </button>
+            </div>
+        </div>
+    )
+}
+
+export default builder
--- src/components/iceCream/iceCream.js
+++ src/components/iceCream/iceCream.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import classes from './iceCream.module.css';
-
+import Scoop from './scoop/scoop';
 const iceCream = () => {
     return (
         <div><div>
             <div className={classes.iceCream}>
                 <p className={classes.cone}></p>
                 {/* Scoop Component */}
+                <Scoop></Scoop>
                 <div className={classes.cherry}></div>
 
             </div>
